81f4f1fa25660e953ed2b859414a4c08035eba28
Lucjan <luqapo@gmail.com>
2020-04-12 10:53:32 +0200

    circle buttons and other styles

--- lib/main.dart
+++ lib/main.dart
@@ -57,92 +57,199 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Container(
         padding: EdgeInsets.only(top: 50.0),
-        decoration: BoxDecoration(color: Colors.grey[300]),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background_1.jpeg'),
+            colorFilter: ColorFilter.mode(
+              Colors.green.withOpacity(0.7),
+              BlendMode.softLight,
+              ),
+            fit: BoxFit.fill
+            ),
+          ),
         child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                IconButton(
+                RaisedButton(
                   onPressed: () => _sendMessage('1+'),
-                  icon: Icon(Icons.keyboard_arrow_up),
-                  iconSize:100,
-                  color: Colors.green[800],
+                  padding: EdgeInsets.all(10.0),
+                  elevation:9.0,
+                  shape: CircleBorder(
+                    side: BorderSide(
+                      color: Colors.grey[800],
+                      ),
+                    ),
+                  child: Icon(
+                      Icons.keyboard_arrow_up,
+                      size:100,
+                      color: Colors.green[800],
+                  ),
+                  color: Colors.black.withOpacity(0.25),
                 ),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: <Widget>[
-                IconButton(
+                RaisedButton(
                   onPressed: () => _sendMessage('3+'),
-                  icon: Icon(Icons.keyboard_arrow_left),
-                  iconSize: 100,
-                  color: Colors.blue[600],
+                  padding: EdgeInsets.all(10.0),
+                  elevation:9.0,
+                  shape: CircleBorder(
+                    side: BorderSide(color: Colors.grey[800]),
+                    ),
+                  child: Icon(
+                    Icons.keyboard_arrow_left,
+                    size:100,
+                    color: Colors.blue[600],
                   ),
-                IconButton(
-                  onPressed: () => _sendMessage('5+'),
-                  icon: Icon(Icons.stop),
-                  iconSize: 80,
-                  color: Colors.red[600],
+                  color: Colors.black.withOpacity(0.25),
                 ),
-                IconButton(
-                  onPressed: () => _sendMessage('4+'),
-                  icon: Icon(Icons.keyboard_arrow_right),
-                  iconSize: 100,
-                  color: Colors.blue[600],
+                RaisedButton(
+                  onPressed: () => _sendMessage('5+'),
+                  padding: EdgeInsets.all(10.0),
+                  elevation:9.0,
+                  shape: CircleBorder(
+                    side: BorderSide(
+                      color: Colors.grey[800],
+                    ),
                   ),
+                  child: Icon(
+                    Icons.stop,
+                    size:100,
+                    color: Colors.red[600],
+                  ),
+                  color: Colors.black.withOpacity(0.25),
+                ),
+                RaisedButton(
+                  onPressed: () => _sendMessage('4+'),
+                  padding: EdgeInsets.all(10.0),
+                  elevation:9.0,
+                  shape: CircleBorder(
+                    side: BorderSide(
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                  child: Icon(
+                    Icons.keyboard_arrow_right,
+                    size:100,
+                    color: Colors.blue[600],
+                  ),
+                  color: Colors.black.withOpacity(0.25),
+                ),
               ],
             ),
-            Row(
+            Expanded( child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: <Widget>[
                 RaisedButton(
                   onPressed: () => _sendMessage('6+'),
-                  child: Text('Heart'),
+                  padding: EdgeInsets.all(10.0),
+                  elevation:9.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25),
+                    side: BorderSide(
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                  child: Text('Heart', style: TextStyle(fontSize: 23),),
                   color: Colors.blue[600],
                   ),
                 RaisedButton(
                   onPressed: () => _sendMessage('7+'),
-                  child: Text('Fire'),
+                  padding: EdgeInsets.all(10.0),
+                  elevation:9.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25),
+                    side: BorderSide(
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                  child: Text('Fire', style: TextStyle(fontSize: 23),),
                   color: Colors.red[600],
                 ),
                 RaisedButton(
                   onPressed: () => _sendMessage('8+'),
-                  child: Text('Skull'),
+                  padding: EdgeInsets.all(10.0),
+                  elevation:9.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25),
+                    side: BorderSide(
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                  child: Text('Skull', style: TextStyle(fontSize: 23),),
                   color: Colors.blue[600],
                   ),
               ],
-            ),
-            Row(
+            )),
+            Expanded( child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: <Widget>[
                 RaisedButton(
                   onPressed: () => _sendMessage('9+'),
-                  child: Text('Hello'),
+                  padding: EdgeInsets.all(10.0),
+                  elevation:9.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25),
+                    side: BorderSide(
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                  child: Text('Hello', style: TextStyle(fontSize: 23),),
                   color: Colors.blue[600],
                   ),
                 RaisedButton(
                   onPressed: () => _sendMessage('10+'),
-                  child: Text('Punch'),
+                  padding: EdgeInsets.all(10.0),
+                  elevation:9.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25),
+                    side: BorderSide(
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                  child: Text('Punch', style: TextStyle(fontSize: 23),),
                   color: Colors.red[600],
                 ),
                 RaisedButton(
                   onPressed: () => _sendMessage('11+'),
-                  child: Text('Dance'),
+                  padding: EdgeInsets.all(10.0),
+                  elevation:9.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25),
+                    side: BorderSide(
+                      color: Colors.grey[800],
+                      ),
+                ),
+                  child: Text('Dance', style: TextStyle(fontSize: 23),),
                   color: Colors.blue[600],
                   ),
               ],
-            ),
+            )),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.end,
               children: <Widget>[
-                IconButton(
-                  onPressed: () => _sendMessage('1+'),
-                  icon: Icon(Icons.home),
-                  iconSize:80,
-                  color: Colors.green[800],
+                RaisedButton(
+                  onPressed: () => _sendMessage('15+'),
+                  padding: EdgeInsets.all(15.0),
+                  elevation:9.0,
+                  shape: CircleBorder(
+                    side: BorderSide(
+                      color: Colors.grey[800],
+                      ),
+                    ),
+                  child: Icon(
+                      Icons.home,
+                      size:80,
+                      color: Colors.yellow[800],
+                  ),
+                  color: Colors.black.withOpacity(0.25),
                 ),
               ],
             ),
