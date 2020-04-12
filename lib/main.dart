cf1adc3b5d37533fae4864ccddfc80f4cdd5d9a0
Lucjan <luqapo@gmail.com>
2020-04-12 18:04:08 +0200

    raised buttons new styles

--- lib/main.dart
+++ lib/main.dart
@@ -56,10 +56,10 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Colors.blue[500],
       ),
       body: Container(
-        padding: EdgeInsets.only(top: 50.0),
+        padding: const EdgeInsets.only(top: 50.0),
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('assets/images/background_1.jpeg'),
+            image: const AssetImage('assets/images/background_1.jpeg'),
             colorFilter: ColorFilter.mode(
               Colors.green.withOpacity(0.7),
               BlendMode.softLight,
@@ -76,7 +76,7 @@ class _MyHomePageState extends State<MyHomePage> {
               children: <Widget>[
                 RaisedButton(
                   onPressed: () => _sendMessage('1+'),
-                  padding: EdgeInsets.all(10.0),
+                  padding: const EdgeInsets.all(8.0),
                   elevation:9.0,
                   shape: CircleBorder(
                     side: BorderSide(
@@ -97,7 +97,7 @@ class _MyHomePageState extends State<MyHomePage> {
               children: <Widget>[
                 RaisedButton(
                   onPressed: () => _sendMessage('3+'),
-                  padding: EdgeInsets.all(10.0),
+                  padding: const EdgeInsets.all(8.0),
                   elevation:9.0,
                   shape: CircleBorder(
                     side: BorderSide(color: Colors.grey[800]),
@@ -111,7 +111,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
                 RaisedButton(
                   onPressed: () => _sendMessage('5+'),
-                  padding: EdgeInsets.all(10.0),
+                  padding: const EdgeInsets.all(8.0),
                   elevation:9.0,
                   shape: CircleBorder(
                     side: BorderSide(
@@ -127,7 +127,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
                 RaisedButton(
                   onPressed: () => _sendMessage('4+'),
-                  padding: EdgeInsets.all(10.0),
+                  padding: const EdgeInsets.all(8.0),
                   elevation:9.0,
                   shape: CircleBorder(
                     side: BorderSide(
@@ -148,42 +148,60 @@ class _MyHomePageState extends State<MyHomePage> {
               children: <Widget>[
                 RaisedButton(
                   onPressed: () => _sendMessage('6+'),
-                  padding: EdgeInsets.all(10.0),
+                  padding: const EdgeInsets.all(20.0),
                   elevation:9.0,
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(25),
+                    borderRadius: BorderRadius.circular(45),
                     side: BorderSide(
                       color: Colors.grey[800],
                     ),
                   ),
-                  child: Text('Heart', style: TextStyle(fontSize: 23),),
-                  color: Colors.blue[600],
+                  child: Text(
+                    'Heart',
+                    style: TextStyle(
+                      fontSize: 23,
+                      color: Colors.blue,
+                      ),
+                    ),
+                  color: Colors.black.withOpacity(0.25),
                   ),
                 RaisedButton(
                   onPressed: () => _sendMessage('7+'),
-                  padding: EdgeInsets.all(10.0),
+                  padding: const EdgeInsets.all(20.0),
                   elevation:9.0,
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(25),
+                    borderRadius: BorderRadius.circular(45),
                     side: BorderSide(
                       color: Colors.grey[800],
                     ),
                   ),
-                  child: Text('Fire', style: TextStyle(fontSize: 23),),
-                  color: Colors.red[600],
+                  child: Text(
+                    'Fire',
+                    style: TextStyle(
+                      fontSize: 23,
+                      color: Colors.blue,
+                      ),
+                    ),
+                  color: Colors.black.withOpacity(0.25),
                 ),
                 RaisedButton(
                   onPressed: () => _sendMessage('8+'),
-                  padding: EdgeInsets.all(10.0),
+                  padding: const EdgeInsets.all(20.0),
                   elevation:9.0,
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(25),
+                    borderRadius: BorderRadius.circular(45),
                     side: BorderSide(
                       color: Colors.grey[800],
                     ),
                   ),
-                  child: Text('Skull', style: TextStyle(fontSize: 23),),
-                  color: Colors.blue[600],
+                  child: Text(
+                    'Skull',
+                    style: TextStyle(
+                      fontSize: 23,
+                      color: Colors.blue,
+                      ),
+                    ),
+                  color: Colors.black.withOpacity(0.25),
                   ),
               ],
             )),
@@ -192,42 +210,60 @@ class _MyHomePageState extends State<MyHomePage> {
               children: <Widget>[
                 RaisedButton(
                   onPressed: () => _sendMessage('9+'),
-                  padding: EdgeInsets.all(10.0),
+                  padding: const EdgeInsets.all(20.0),
                   elevation:9.0,
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(25),
+                    borderRadius: BorderRadius.circular(45),
                     side: BorderSide(
                       color: Colors.grey[800],
                     ),
                   ),
-                  child: Text('Hello', style: TextStyle(fontSize: 23),),
-                  color: Colors.blue[600],
+                  child: Text(
+                    'Hello',
+                    style: TextStyle(
+                      fontSize: 23,
+                      color: Colors.blue,
+                      ),
+                    ),
+                  color: Colors.black.withOpacity(0.25),
                   ),
                 RaisedButton(
                   onPressed: () => _sendMessage('10+'),
-                  padding: EdgeInsets.all(10.0),
+                  padding: const EdgeInsets.all(20.0),
                   elevation:9.0,
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(25),
+                    borderRadius: BorderRadius.circular(45),
                     side: BorderSide(
                       color: Colors.grey[800],
                     ),
                   ),
-                  child: Text('Punch', style: TextStyle(fontSize: 23),),
-                  color: Colors.red[600],
+                  child: Text(
+                    'Punch',
+                    style: TextStyle(
+                      fontSize: 23,
+                      color: Colors.blue,
+                      ),
+                    ),
+                  color: Colors.black.withOpacity(0.25),
                 ),
                 RaisedButton(
                   onPressed: () => _sendMessage('11+'),
-                  padding: EdgeInsets.all(10.0),
+                  padding: const EdgeInsets.all(20.0),
                   elevation:9.0,
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(25),
+                    borderRadius: BorderRadius.circular(45),
                     side: BorderSide(
                       color: Colors.grey[800],
                       ),
                 ),
-                  child: Text('Dance', style: TextStyle(fontSize: 23),),
-                  color: Colors.blue[600],
+                  child: Text(
+                    'Dance',
+                    style: TextStyle(
+                      fontSize: 23, color:
+                      Colors.blue,
+                      ),
+                    ),
+                  color: Colors.black.withOpacity(0.25),
                   ),
               ],
             )),
@@ -237,7 +273,7 @@ class _MyHomePageState extends State<MyHomePage> {
               children: <Widget>[
                 RaisedButton(
                   onPressed: () => _sendMessage('15+'),
-                  padding: EdgeInsets.all(15.0),
+                  padding: const EdgeInsets.all(12.0),
                   elevation:9.0,
                   shape: CircleBorder(
                     side: BorderSide(
@@ -246,7 +282,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                   child: Icon(
                       Icons.home,
-                      size:80,
+                      size:70,
                       color: Colors.yellow[800],
                   ),
                   color: Colors.black.withOpacity(0.25),
